875cedab85b5e09a277690db84f07860213db858
Marco Olivares <marco.olivares@wizeline.com>
2020-06-10 14:58:54 -0500

    POM Class for the Add People to Channel Modal

---
+++ cypress/Pages/Channels/AddPeopleToChannelModal.js
@@ -0,0 +1,16 @@
+class AddPeopleToChannelModal{
+
+    lblChannelName(){ return cy.get('[role="dialog"] [data-qa="inline_channel_entity__name"]'); }
+    btnCloseModal(){ return cy.get('[data-qa="sk_close_modal_button"]'); }
+
+    closeAddPeopleToChannelModal(){
+        this.btnCloseModal().click();
+    }
+
+    getChannelName(callback){
+        this.lblChannelName().then((selector) => {
+             callback(selector.text());
+        });
+    }
+
+} export default AddPeopleToChannelModal;
