ea729725b48ab5ccfcb19947092dc688aaa94d70
Marco Olivares <marco.olivares@wizeline.com>
2020-06-10 15:03:35 -0500

    This is the POM Class that contains the elements needed to create a new channel once the modal is open

---
+++ cypress/Pages/Channels/CreateChannelModal.js
@@ -0,0 +1,12 @@
+class CreateChannelModal{ //Extending the Left Navigation Bar so it's ready to be used when needing the Modal
+
+    txtChannelName(){ return cy.get('[data-qa="channel-name-input"]').clear(); } //Clears the field      
+    txtChannelDescription(){ return cy.get('[data-qa="channel_create_modal_purpose"] p').clear(); }//Clears the field
+    btnCreateChannel(){ return cy.get('[data-qa="channel_create_modal_cta"]'); }
+
+    createNewChannel(incommingChannelName, incommingChannelDescription = 'Default: Created by Automation'){
+        this.txtChannelName().type(incommingChannelName, {force:true});
+        this.txtChannelDescription().type(incommingChannelDescription);
+        this.btnCreateChannel().click();
+    }
+} export default CreateChannelModal;
