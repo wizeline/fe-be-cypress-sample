214227b46be703cdff5e9f72d20520a766fe9c15
Marco Olivares <marco.olivares@wizeline.com>
2020-06-10 15:07:54 -0500

    Deleting sample files

--- cypress/integration/examples/writeElementWithGet.js
+++
@@ -1,9 +0,0 @@
-describe('Formas de encontrar un elemento', function() {
-    it('Search bar', function() {
-        cy.visit('http://automationpractice.com/index.php')
-        cy.get('.search_query.form-control.ac_input').type('Hi') //By class
-        cy.get('#search_query_top').type('First test') //By id
-        cy.get('[name=search_query]').type('Test 2') //By name
-        cy.get('[placeholder=Search]').clear() //By class
-    })
-});
