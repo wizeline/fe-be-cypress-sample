c52e24dc7463d0108813e1ce68cda9dd90dfc86e
Marco Olivares <marco.olivares@wizeline.com>
2020-06-16 16:08:25 -0500

    Added more selectors

--- cypress/Pages/Channels/AddPeopleToChannelModal.js
+++ cypress/Pages/Channels/AddPeopleToChannelModal.js
@@ -7,6 +7,22 @@ class AddPeopleToChannelModal {
     return cy.get('[data-qa="sk_close_modal_button"]');
   }
 
+  btnDone() {
+    cy.get('[data-qa="invite_to_workspace_submit_button"]');
+  }
+
+  rdioAddSpecificPeople() {
+    cy.get('[data-qa="add-specific-members-option"]');
+  }
+
+  txtFindUserName() {
+    return cy.get('[data-qa="invite_to_workspace_select-input"]');
+  }
+
+  lstSearchResult() {
+    return cy.get('[data-qa="member-entity__primary-name"]');
+  }
+
   closeAddPeopleToChannelModal() {
     this.btnCloseModal().click();
   }
@@ -15,6 +31,17 @@ class AddPeopleToChannelModal {
     this.lblChannelName().then((selector) => {
       callback(selector.text());
     });
+  }
+
+  addUserToChannel(incommingUserName) {
+    this.rdioAddSpecificPeople().click();
+    this.txtFindUserName().type(incommingUserName);
+    this.lstSearchResult().each((user) => {
+      if (user.text() === incommingUserName) {
+        user.click();
+      }
+    });
+    this.btnDone().click();
   }
 }
 export default AddPeopleToChannelModal;
