f588dc0d9a181f893fa0d5800e29e869b9b2928d
Marco Olivares <marco.olivares@wizeline.com>
2020-06-30 16:39:19 -0500

    Updated the File URL

--- cypress/Utils/PreTest.sh
+++ cypress/Utils/PreTest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # replacing placeholder strings with values from CircleCI ENV Variables
 
-sed -i -e 's/$BACKEND_CONVERSATION_ID/'${BACKEND_CONVERSATION_ID//\//\\/}'/g' cypress.json # TO TEST THE SCRIPT
+sed -i -e 's/$BACKEND_CONVERSATION_ID/'${BACKEND_CONVERSATION_ID//\//\\/}'/g' cypress/backend/slackEnvironment.postman_environment.json # TO TEST THE SCRIPT
+
