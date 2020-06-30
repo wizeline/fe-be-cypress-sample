d7e4fa2d364e59e21243c10a001f03bd2ab44842
Marco Olivares <marco.olivares@wizeline.com>
2020-06-30 16:19:40 -0500

    Test the ID substitution

--- cypress/Utils/PreTest.sh
+++ cypress/Utils/PreTest.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # replacing placeholder strings with values from CircleCI ENV Variables
 
-sed -i -e 's/$CYPRESS_SLACK_URL/'${CYPRESS_SLACK_URL//\//\\/}'/g' cypress.json # for the baseURL
-sed -i -e 's/$CYPRESS_PROJECT_ID/'${CYPRESS_PROJECT_ID//\//\\/}'/g' cypress.json # for the projectID
-sed -i -e 's/$CYPRESS_DASHBOARD_KEY/'${CYPRESS_DASHBOARD_KEY//\//\\/}'/g' package.json # for the DashboardID
+sed -i -e 's/$BACKEND_CONVERSATION_ID/'${BACKEND_CONVERSATION_ID//\//\\/}'/g' cypress.json # TO TEST THE SCRIPT
